681d58e67ff5ecbb2d5a282002c688bfd43122c4
Jujiwon <zuzione@gmail.com>
2019-05-06 22:23:46 +0900

    Initial Commit

---
+++ MyGame_3/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyGame_3
+//
+//  Created by JiWon Ju on 06/05/2019.
+//  Copyright © 2019 Ju Hye Won. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
